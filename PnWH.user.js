bed17997ae15d59eb1acc6cdecba5fdc9499e4d8
Ryahn <ryancarr10@gmail.com>
2017-11-09 14:24:33 -0500

    Testing some features

--- PnWH.user.js
+++ PnWH.user.js
@@ -3,7 +3,7 @@
 // @author					Ryahn aka Praximus Cladius
 // @description			Adds useful functions to the Game Politics and War
 // @include					https://politicsandwar.com/*
-// @version					0.7.3
+// @version					0.7.4
 // @updateURL				https://github.com/Ryahn/PnWH/raw/master/PnWH.user.js
 // @downloadURL			https://github.com/Ryahn/PnWH/raw/master/PnWH.user.js
 // @grant						GM_setValue
@@ -19,4 +19,21 @@ var stamp = d.getTime();
 var lastUpdate = GM_getValue("lastUpdate", 0);
 var pwhThisVersion = GM_info.script.version;
 
-console.log("Script Version: " + pwhThisVersion + "\nUpdate: " + lastUpdate + "\nStamp: " + stamp);
+
+//Checks for update once every 10 minutes
+if(stamp > (lastUpdate + 600000)){
+	GM_setValue("lastUpdate", stamp);
+	GM_xmlhttpRequest({
+		method: "GET",
+		url: "https://greasyfork.org/en/scripts/34969-politics-and-war-helper-2",
+		headers: {
+			"User-Agent": "Mozilla/5.0",
+		},
+		onload: function(response) {
+			GM_setValue("pwhCurrentVersion", jQuery(response.responseText).find("dd[class='script-show-version']").text());
+		}
+	});
+}
+var pwhCurrentVersion = GM_getValue("pwhCurrentVersion");
+
+console.log("Script Version: " + pwhThisVersion + "\nUpdate: " + lastUpdate + "\nStamp: " + stamp + "\nCurrent: " + pwhCurrentVersion);
